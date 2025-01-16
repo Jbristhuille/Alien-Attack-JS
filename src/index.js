a26655d119b6fea0534b6603b247e02dee25fcbb
Jbristhuille <jbristhuille@gmail.com>
2025-01-16 17:24:16 +0100

    Setup basic

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/******************************************************************************
+ * @Author                : Jbristhuille<jbristhuille@gmail.com>              *
+ * @CreatedDate           : 2025-01-16 17:17:21                               *
+ * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>              *
+ * @LastEditDate          : 2025-01-16 17:17:21                               *
+ *****************************************************************************/
+
