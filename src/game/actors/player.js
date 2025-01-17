dd6763495502ffc5c29eac1cb029a8ac700ce90a
Jbristhuille <jbristhuille@gmail.com>
2025-01-17 12:34:31 +0100

    transform player code to class

--- src/game/actors/player.js
+++ src/game/actors/player.js
@@ -2,37 +2,43 @@
  * @Author                : Jbristhuille<jbristhuille@gmail.com>              *
  * @CreatedDate           : 2025-01-17 11:04:45                               *
  * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>              *
- * @LastEditDate          : 2025-01-17 12:31:06                               *
+ * @LastEditDate          : 2025-01-17 12:34:22                               *
  *****************************************************************************/
 
-function playerInit() {
-  const speed = 10;
-  const player = new Actor({
-    width: 50,
-    height: 50,
-    color: "red"
-  });
+class Player {
+  constructor() {
+    this.speed = 10;
+    this.player = new Actor({
+      width: 50,
+      height: 50,
+      color: "red"
+    });
+  
+    this.player.spawn(game, [
+      game.offsetWidth/2 - this.player.width/2, 
+      game.offsetHeight - this.player.height - 20
+    ]);
 
-  player.spawn(game, [
-    game.offsetWidth/2 - player.options.width/2, 
-    game.offsetHeight - player.options.height - 20
-  ]);
+    this.loop();
+  }
 
-  setInterval(() => { // Game loop
-    if (isKeyDown("q")) {
-      if (player.pos.x > 0) {
-        player.setPosition([player.position.x + speed * -1, player.position.y]);
+  loop() {
+    setInterval(() => {
+      if (isKeyDown("q")) {
+        if (this.player.position.x > 0) {
+          this.player.setPosition([this.player.position.x + this.speed * -1, this.player.position.y]);
+        }
       }
-    }
-
-    if (isKeyDown("d")) {
-      if (player.pos.x < game.offsetWidth - player.options.width) {
-        player.setPosition([player.position.x + speed * 1, player.position.y]);
+  
+      if (isKeyDown("d")) {
+        if (this.player.position.x < game.offsetWidth - this.player.width) {
+          this.player.setPosition([this.player.position.x + this.speed * 1, this.player.position.y]);
+        }
       }
-    }
-
-    if (isKeyDown(" ")) {
-      new Bullet([player.position.x + player.width/2, player.position.y]);
-    }
-  }, GAME_LOOP);
+  
+      if (isKeyDown(" ")) {
+        new Bullet([this.player.position.x + this.player.width/2, this.player.position.y]);
+      }
+    }, GAME_LOOP);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@
  * @Author                : Jbristhuille<jbristhuille@gmail.com>              *
  * @CreatedDate           : 2025-01-16 17:17:21                               *
  * @LastEditors           : Jbristhuille<jbristhuille@gmail.com>              *
- * @LastEditDate          : 2025-01-17 11:04:37                               *
+ * @LastEditDate          : 2025-01-17 12:33:55                               *
  *****************************************************************************/
 
 const game = document.querySelector("#game");
 
 if (game) {
-  playerInit();
+  new Player();
 } else {
   console.error("Game zone not instanciate...");
 }
